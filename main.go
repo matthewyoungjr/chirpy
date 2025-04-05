b2edc41bc8cfa3da32f4efe97f4928ba36ef3322
Matthew <mattyoungjnr@gmail.com>
2025-04-05 15:57:33 +0000

    Added metrics middleware for logging number of requests

--- main.go
+++ main.go
@@ -1,20 +1,59 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	serverHits atomic.Int32
+}
 
 func main() {
 	mux := http.NewServeMux()
 
-	server := http.Server{
+	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("This is the response for the main dir...."))
+	config := &apiConfig{}
+
+	mux.Handle("/app/", config.MetricsMiddleWare(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		count := fmt.Sprintf("Hits: %d", config.serverHits.Load())
+		fmt.Println(count)
+		w.Write([]byte(count))
 	})
 
-	server.ListenAndServe()
+	mux.Handle("/reset", config.Reset())
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "OK %d", http.StatusOK)
+	})
+
+	log.Fatal(server.ListenAndServe())
+
+}
+
+func (a *apiConfig) MetricsMiddleWare(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		a.serverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+
+}
+func (a *apiConfig) Reset() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		a.serverHits.Swap(0)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Count has been reset successfully."))
+	})
+
 }
