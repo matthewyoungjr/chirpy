d01146241cc8e1cdbf28d06db1e981b1787a6a62
Matthew <mattyoungjnr@gmail.com>
2025-04-06 11:24:25 +0000

    add admin namespace & html templating to metrics endpoint

--- main.go
+++ main.go
@@ -23,14 +23,21 @@ func main() {
 
 	mux.Handle("/app/", config.MetricsMiddleWare(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 
-	mux.HandleFunc("GET /api/metrics", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		count := fmt.Sprintf("Hits: %d", config.serverHits.Load())
-		fmt.Println(count)
-		w.Write([]byte(count))
+		w.Write([]byte(fmt.Sprintf(`
+	<html>
+
+		<body>
+		<h1>Welcome, Chirpy Admin</h1>
+		<p>Chirpy has been visited %d times!</p>
+		</body>
+
+	</html>
+	`, config.serverHits.Load())))
 	})
 
-	mux.Handle("POST /api/reset", config.Reset())
+	mux.Handle("POST /admin/reset", config.Reset())
 
 	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
