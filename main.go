ed95ce22cd018876a0ee1de7b2e94e8ee586cf78
Matthew <mattyoungjnr@gmail.com>
2025-04-10 13:48:41 +0000

    Move handlers to handlers.go

--- main.go
+++ main.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
-	"sync/atomic"
 )
-
-type apiConfig struct {
-	serverHits atomic.Int32
-}
 
 func main() {
 	mux := http.NewServeMux()
@@ -41,74 +34,10 @@ func main() {
 
 	mux.Handle("POST /admin/reset", config.Reset())
 
-	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "OK %d", http.StatusOK)
-	})
+	mux.HandleFunc("GET /api/healthz", Healthz)
 
-	mux.HandleFunc("/api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			errmsg := Error{Error: "Method not allowed !"}
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			json.NewEncoder(w).Encode(errmsg)
-			return
-		}
-
-		var requestBody RequestBody
-		json.NewDecoder(r.Body).Decode(&requestBody)
-
-		if len(requestBody.Body) > 140 {
-			errmsg := &Error{Error: "Chirp is too long"}
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(&errmsg)
-			return
-		}
-
-		profane := []string{"kerfuffle", "sharbert", "fornax"}
-		cleaned := requestBody.Body
-		wasCleaned := false
-
-		for _, p := range profane {
-			if strings.Contains(cleaned, p) {
-				cleaned = strings.ReplaceAll(cleaned, p, "****")
-				wasCleaned = true
-			}
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-
-		if wasCleaned {
-			newBody := Cleaned{CleanedBody: cleaned}
-			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(newBody)
-			return
-		}
-
-		response := Valid{Valid: true}
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(&response)
-
-	})
+	mux.HandleFunc("/api/validate_chirp", ValidateChirp)
 
 	log.Fatal(server.ListenAndServe())
-
-}
-
-func (a *apiConfig) MetricsMiddleWare(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		a.serverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-
-}
-func (a *apiConfig) Reset() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		a.serverHits.Swap(0)
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Count has been reset successfully."))
-	})
 
 }
